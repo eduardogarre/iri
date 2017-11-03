135afba68bf2f151f14ed67c02871807d9e0e0b0
eduardogarre <eduardo.garre@gmail.com>
2017-11-04 00:16:34 +0100

    Nueva función @escribe()

--- prueba.bat
+++ prueba.bat
@@ -2,18 +2,49 @@
 
 echo op:sum
 .\iri.exe .\pruebas\op_sum.ri
-
 echo.
 
 echo op:res
 .\iri.exe .\pruebas\op_res.ri
-
 echo.
 
 echo op:mul
 .\iri.exe .\pruebas\op_mul.ri
-
 echo.
 
 echo op:div
 .\iri.exe .\pruebas\op_div.ri
+echo.
+
+echo op:llama
+echo.
+
+echo op:cmp
+echo.
+
+echo op:conv
+echo.
+
+echo op:slt
+echo.
+
+echo op:phi
+echo.
+
+echo op:rsrva
+echo.
+
+echo op:lee
+echo.
+
+echo op:guarda
+echo.
+
+echo op:leeval
+echo.
+
+echo op:ponval
+echo.
+
+echo op:ret
+echo.
